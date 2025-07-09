1419cffe0e922fb7994c5a5e10f3263e39c4dd26
Rahf Ayman <rahf.family@gmail.com>
2025-07-09 23:05:22 +0300

    Feat : add initial migration and fixed country , weapon seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Country;
 
 return new class extends Migration
 {
@@ -14,9 +15,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignIdFor(Country::class);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['government','general', 'admin' ])->default('general');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_07_09_192821_create_weapons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Country;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\WeaponType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weapons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignIdFor(WeaponType::class)
+                ->constrained()
+                ->onDelete('cascade');
+            $table->foreignIdFor(Country::class)
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('is_available')->default(true);    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weapons');
+    }
+};
