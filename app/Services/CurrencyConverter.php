8046377893c662cb392221807449a7fe4fa8e96d
Badr Mohamed <20912023100172@fci.zu.edu.eg>
2025-07-17 00:21:55 +0300

    make the CurrencyConverter.php

---
+++ app/Services/CurrencyConverter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+class CurrencyConverter
+{
+    private $from_dolar_to_curr = [];
+    public function __construct()
+    {
+        $this->from_dolar_to_curr =
+            [
+                'Germany' => 0.5,
+                'England' => 1.5,
+                'Soviet_Union' => 0.2,
+                'Switzerland' => 1.0
+            ];
+    }
+
+    public function convert($amount, $country)
+    {
+        $converted = $amount * $this->from_dolar_to_curr[$country];
+        return $converted;
+    }
+}
